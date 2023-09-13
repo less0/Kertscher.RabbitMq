ca7edc9fb4d0a39b646dbef4d55ab250a3d14e80
Paul Kertscher <paul.kertscher+code@gmail.com>
2023-09-13 16:54:13 +0200

    Added tests for RPC calls

--- Kertscher.RabbitMq.Rpc.Tests/RpcClientBaseTests.cs
+++ Kertscher.RabbitMq.Rpc.Tests/RpcClientBaseTests.cs
@@ -1,3 +1,4 @@
+using System.Text;
 using FluentAssertions;
 using Xunit;
 
@@ -9,8 +10,20 @@ public class RpcClientBaseTests
     [Fact]
     public async Task CallMethod_CancellationTokenTimesOut_ThrowsTaskCanceledException()
     {
-        var componentUnderTest = new RpcClient("localhost", "test");
+        using RpcClient componentUnderTest = new("localhost", "test");
         var exception = await Record.ExceptionAsync(async () => await componentUnderTest.GetSomething());
         exception.Should().BeOfType<TaskCanceledException>();
+    }
+
+    [Fact]
+    public async Task CallMethod_ReceivesResult_ResultIsReturned()
+    {
+        using RpcClient componentUnderTest = new("localhost", "test");
+        using RpcServer server = new("localhost", "test");
+
+        var response = await componentUnderTest.GetSomething();
+        var responseText = Encoding.UTF8.GetString(response);
+
+        responseText.Should().Be("Something");
     }
 }
---
+++ Kertscher.RabbitMq.Rpc.Tests/RpcServer.cs
@@ -0,0 +1,54 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Kertscher.RabbitMq.Rpc.Tests;
+
+public class RpcServer : IDisposable
+{
+    private IConnection? _connection;
+    private IModel? _channel;
+    private EventingBasicConsumer? _consumer;
+    private readonly string? _queueName;
+
+    public RpcServer(string hostName, string exchange)
+    {
+        ConnectionFactory connectionFactory = new()
+        {
+            HostName = hostName
+        };
+        _connection = connectionFactory.CreateConnection();
+        _channel = _connection.CreateModel();
+        
+        _channel.ExchangeDeclare(exchange, ExchangeType.Direct);
+        _queueName = _channel.QueueDeclare().QueueName;
+        
+        _channel.QueueBind(_queueName, exchange, "getSomething");
+
+        _consumer = new(_channel);
+        _consumer.Received += ConsumerOnReceived;
+        _channel.BasicConsume(_consumer, _queueName, autoAck: false);
+    }
+    
+    public void Dispose()
+    {
+        _connection?.Dispose();
+        _channel?.Dispose();
+
+        if (_consumer != null)
+        {
+            _consumer.Received -= ConsumerOnReceived;
+        }
+    }
+
+    private void ConsumerOnReceived(object? sender, BasicDeliverEventArgs e)
+    {
+        var basicProperties = _channel.CreateBasicProperties();
+        basicProperties.CorrelationId = e.BasicProperties.CorrelationId;
+
+        var message = Encoding.UTF8.GetBytes("Something");
+        _channel.BasicPublish(string.Empty, e.BasicProperties.ReplyTo, basicProperties, message);
+        
+        _channel.BasicAck(e.DeliveryTag, false);
+    }
+}
