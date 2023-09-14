4f97fd8a76bb4f5c1abba59eea80eba847252c9d
Paul Kertscher <paul.kertscher+code@gmail.com>
2023-09-14 08:55:50 +0200

    Added ControllerResolver scaffold

---
+++ Kertscher.RabbitMq.Rpc/ControllerResolver.cs
@@ -0,0 +1,14 @@
+﻿namespace Kertscher.RabbitMq.Rpc;
+
+internal class ControllerResolver
+{
+    internal void RegisterController<T>()
+    {
+        throw new NotImplementedException();
+    }
+
+    internal byte[] CallMethod(string methodName, byte[] data)
+    {
+        throw new NotImplementedException();
+    }
+}
